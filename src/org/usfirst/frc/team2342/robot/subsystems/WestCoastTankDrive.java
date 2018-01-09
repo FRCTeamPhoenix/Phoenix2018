d63e5e23c55558d3127fa06fde8bba50093c37f0
Stephan Lensky <stephan.lensky@gmail.com>
2018-01-09 18:52:54 -0500

    Add Subsystem abstract class and stub for WestCoastTankDrive

---
+++ src/org/usfirst/frc/team2342/robot/subsystems/WestCoastTankDrive.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team2342.robot.subsystems;
+
+import org.usfirst.frc.team2342.loops.Looper;
+
+public class WestCoastTankDrive extends Subsystem {
+
+    @Override
+    public void outputToSmartDashboard() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void zeroSensors() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void registerEnabledLoops(Looper enabledLooper) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}

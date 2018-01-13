bbd1c899346749b4b6a1e2e34f53d81ee9c3844f
akhi <akhi@10.10.60.19>
2018-01-13 13:31:23 -0500

    Added a PCM handler class

--- src/org/usfirst/frc/team2342/robot/Robot.java
+++ src/org/usfirst/frc/team2342/robot/Robot.java
@@ -1,20 +1,36 @@
 package org.usfirst.frc.team2342.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive
  * class.
  */
 public class Robot extends SampleRobot {
-
+	PCMHandler PCM;
+	Joystick stick;
     public Robot() {
-
+PCM = new PCMHandler(5);
+stick = new Joystick(0);
     }
 
     @Override
     public void operatorControl() {
-
+    	//teliopInit
+    	
+    	while (isEnabled()) {
+    		//teliopPeriodic
+    	if (stick.getRawButton(8)) {
+    		PCM.setHighGear(true);
+    		PCM.setLowGear(false);
+    	}
+    	else {
+    		PCM.setHighGear(false);
+    		PCM.setLowGear(true);
+    	}
+    	}
     }
 
     @Override

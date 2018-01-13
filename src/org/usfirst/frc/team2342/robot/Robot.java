e67acec71ff5e87a67fc70883cebfc64bc556f60
UPIMAN73 <joshcalzadi@gmail.com>
2018-01-13 13:49:06 -0500

    Test run for the drive train
    Added some test code for the smart talon, and now you can drive the drivetrain with this code.

--- src/org/usfirst/frc/team2342/robot/Robot.java
+++ src/org/usfirst/frc/team2342/robot/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2342.robot;
 
 import org.usfirst.frc.team2342.robot.talons.SmartTalon;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
 
 /**
@@ -9,8 +10,12 @@ import edu.wpi.first.wpilibj.SampleRobot;
  * class.
  */
 public class Robot extends SampleRobot {
-
-	SmartTalon talon = new SmartTalon(1);
+	Joystick joystick1 = new Joystick(0);
+	Joystick joystick2 = new Joystick(1);
+	SmartTalon talon1 = new SmartTalon(1);
+	SmartTalon talon2 = new SmartTalon(2);
+	SmartTalon talon3 = new SmartTalon(3);
+	SmartTalon talon4 = new SmartTalon(4);
 	
     public Robot() {
 
@@ -18,12 +23,21 @@ public class Robot extends SampleRobot {
 
     @Override
     public void operatorControl() {
-
+    	double y = joystick1.getY();
+    	double y2 = joystick2.getY();
+    	double speedv = 0.5;
     	while(isEnabled()){
-    		talon.goVoltage(0.3);
+    		y = joystick1.getY();
+    		y2 = joystick2.getY();
+    		talon1.goVoltage(-speedv*y);
+    		talon2.goVoltage(speedv*y2);
+    		talon3.goVoltage(-speedv*y);
+    		talon4.goVoltage(speedv*y2);
     	}
-    	talon.goVoltage(0);
-    	
+    	talon1.goVoltage(0);
+    	talon2.goVoltage(0);
+    	talon3.goVoltage(0);
+    	talon4.goVoltage(0);
     }
 
     @Override
@@ -33,7 +47,16 @@ public class Robot extends SampleRobot {
 
     @Override
     public void test() {
-
+    	while(isEnabled()){
+    		talon1.goVoltage(0.3);
+    		talon2.goVoltage(-0.3);
+    		talon3.goVoltage(0.3);
+    		talon4.goVoltage(-0.3);
+    	}
+    	talon1.goVoltage(0);
+    	talon2.goVoltage(0);
+    	talon3.goVoltage(0);
+    	talon4.goVoltage(0);
     	
     }
 }

77154183d43173d99875d09430742ffb7975c136
Scout1297 <justin.ruiz@asdnh.org>
2018-01-20 14:03:58 -0500

    ActionList now has methods for calling an emergancy stop and starting it back up again.

--- src/ActionList.java
+++ src/ActionList.java
@@ -1,3 +1,37 @@
+import java.util.*;
 
-
-
+public class ActionList{
+	
+	private ArrayList<Action> actions;
+	
+	private boolean emergancyStop = false;
+	
+	public ActionList(){
+		
+	}
+	
+	//Execute all actions who's conditions have been satisfied
+	public void execute(){
+		
+		for(Action action: actions){
+			
+			if(!emergancyStop)
+				action.run();
+			
+			else
+				action.stop();
+		}
+	}
+	
+	public void stop(){
+		
+		emergancyStop = true;
+	}
+	
+	public void start(){
+		
+		emergancyStop = false;
+	}
+	
+	
+}

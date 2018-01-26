8c7626435f7e9d52b0fee83277147abb45d7f769
BryceCzuba <bryce.czuba@asdnh.org>
2018-01-25 20:50:15 -0500

    Changed BoxManipulator to include PCM and PCMHandler.

--- src/org/usfirst/frc/team2342/robot/subsystems/BoxManipulator.java
+++ src/org/usfirst/frc/team2342/robot/subsystems/BoxManipulator.java
@@ -3,39 +3,40 @@ package org.usfirst.frc.team2342.robot.subsystems;
 import org.usfirst.frc.team2342.loops.Looper;
 import org.usfirst.frc.team2342.robot.PCMHandler;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class BoxManipulator extends Subsystem {
 	private TalonSRX talon1;
 	private TalonSRX talon2;
-	private PCMHandler PCM1;
-	private PCMHandler PCM2;
+	private PCMHandler PCM;
 	
 	private final int PidLoopIndex = 0;
 	private final int PidTimeOutMs = 10;
 	private final boolean SensorPhase = true;
 	private final boolean InvertMotor = false;
 	
-	public BoxManipulator(TalonSRX talon1, TalonSRX talon2) {
+	public BoxManipulator(TalonSRX talon1, TalonSRX talon2, PCMHandler PCM) {
 		this.talon1 = talon1;
 		this.talon2 = talon2;
 		this.talon2.follow(talon1);
+		this.PCM = PCM;
 	}
 	
-	public void openManipulator() {
-		talon1.goVoltage(0);
+	public void closeManipulator(double current) {
+		
 	}
 	
-	public void closeManipulat0or() {
-		talon1.goVoltage(0);
+	public void openManipulator(double current) {
+		/*PCM.drivePiston();*/
 	}
 	
-	public void pullBox() {
-		talon1.goVoltage(0);
+	public void pneumatic() {
+		
 	}
 	
-	public void pushBox() {
-		talon1.goVoltage(0);
+	public void distance() {
+		
 	}
 	
 	@Override
@@ -46,8 +47,8 @@ public class BoxManipulator extends Subsystem {
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		talon1.set(ControlMode.Current, 0.0);
+		PCM.set(ControlMode.Current, 0.0);
 	}
 
 	@Override

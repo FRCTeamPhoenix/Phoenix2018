71973b398646465f0f9e98c11217e95926473f46
Stephan Lensky <stephan.lensky@gmail.com>
2018-01-25 19:20:28 -0500

    Fix Test class to use new Json reading method

--- src/org/usfirst/frc/team2342/json/Test.java
+++ src/org/usfirst/frc/team2342/json/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		try {
 			//JsonHelper.WriteConfigMethod();
-			Json config = JsonHelper.readConfigMethod();
+			Json config = JsonHelper.getConfig();
 			System.out.println(config.talons.get(0).maxForwardSpeed);
 		} catch(Exception e) {e.printStackTrace();}
 	}

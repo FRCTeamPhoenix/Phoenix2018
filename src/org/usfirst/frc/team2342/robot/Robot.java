9b0c8393090c02db0d6263cdeee9bd2481205c44
Scout1297 <justin.ruiz@asdnh.org>
2018-02-15 21:12:02 -0500

    Camera basis

--- src/org/usfirst/frc/team2342/robot/Robot.java
+++ src/org/usfirst/frc/team2342/robot/Robot.java
@@ -1,19 +1,24 @@
 package org.usfirst.frc.team2342.robot;
 
-import org.usfirst.frc.team2342.commands.DriveDistance;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team2342.commands.DriveGamepad;
 import org.usfirst.frc.team2342.robot.subsystems.WestCoastTankDrive;
 import org.usfirst.frc.team2342.util.Constants;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.opencv.core.CvType;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive
@@ -22,7 +27,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
-	Joystick gamepad = new Joystick(0);
+	/*Joystick gamepad = new Joystick(0);
 	PCMHandler PCM = new PCMHandler(11);
 	WPI_TalonSRX talonFR = new WPI_TalonSRX(Constants.RIGHT_MASTER_TALON_ID);
 	WPI_TalonSRX talonFL = new WPI_TalonSRX(Constants.LEFT_MASTER_TALON_ID);
@@ -32,12 +37,12 @@ public class Robot extends IterativeRobot {
 	WPI_TalonSRX talonIntakeRight = new WPI_TalonSRX(Constants.TALON_INTAKE_RIGHT);
 	WPI_TalonSRX talonIntakeLeft = new WPI_TalonSRX(Constants.TALON_INTAKE_LEFT);
 	WPI_TalonSRX talonTip = new WPI_TalonSRX(Constants.TALON_TIP);
-	Solenoid solenoidLowGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_LOWGEAR);
-	Solenoid solenoidHighGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_HIGHGEAR);
-	Solenoid solenoid1 = new Solenoid(Constants.PCM_CAN_ID, Constants.PCM_BOX_MANIPULATOR);
+	//Solenoid solenoidLowGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_LOWGEAR);
+	//Solenoid solenoidHighGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_HIGHGEAR);
+	//Solenoid solenoid1 = new Solenoid(Constants.PCM_CAN_ID, Constants.PCM_BOX_MANIPULATOR);
 	WestCoastTankDrive westCoast = new WestCoastTankDrive(PCM, talonFL, talonFR, talonBL, talonBR);
 	Joystick joystickR = new Joystick(2);
-	Joystick joystickL = new Joystick(1);
+	Joystick joystickL = new Joystick(1);*/
 
 	public Robot() {
 		//PCM.turnOn();
@@ -48,16 +53,35 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
-		System.out.println("TELEOP MODE INIT");
+		/*System.out.println("TELEOP MODE INIT");
 		PCM.turnOn();
 		Command driveJoystick = new DriveGamepad(gamepad, westCoast);
 		Scheduler.getInstance().add(driveJoystick);
 		westCoast.setGyroControl(false);
-		westCoast.debug = false;
+		westCoast.debug = false;*/
+		
+		new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("USB Camera 6", 0);
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo(camera);
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur Current", 320, 240);
+            
+            Mat source = new Mat(320, 240, CvType.CV_32FC3);
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+            	cvSink.setEnabled(true);
+                cvSink.grabFrame(source);
+                outputStream.putFrame(output);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                
+            }
+        }).start();
 	}
     
     public void teleopPeriodic() {
-    	Scheduler.getInstance().run();
+    	/*Scheduler.getInstance().run();
     	//Drive with joystick control in velocity mode
 		westCoast.outputToSmartDashboard();
 		//Buttons 8 & 9 or (gamepad) 5 & 6 are Low & High gear, respectively
@@ -85,42 +109,42 @@ public class Robot extends IterativeRobot {
 		// WPI_TalonSRX talon1 = new WPI_TalonSRX(0);
 		// WPI_TalonSRX talon2 = new WPI_TalonSRX(1);
 		// boxManipulator = new BoxManipulator(talon1, talon2, PCM);
-		// cascadeElevator = new CascadeElevator(talon1, talon2);
+		// cascadeElevator = new CascadeElevator(talon1, talon2);*/
 	}
     
 	public void disabledInit() {
-		westCoast.setVelocity(0.0d, 0.0d);
+		/*westCoast.setVelocity(0.0d, 0.0d);
 		westCoast.zeroSensors();
-		Scheduler.getInstance().removeAll();
+		Scheduler.getInstance().removeAll();*/
 	}
 
 	public void autonomousInit() {
-		//Command goForward = new DriveForward(20, westCoast, 6.0 * Constants.TALON_SPEED_RPS);
+		/*//Command goForward = new DriveForward(20, westCoast, 6.0 * Constants.TALON_SPEED_RPS);
 		//Scheduler.getInstance().add(goForward);
 		westCoast.goArc(8, 90, 0.425, 0.5, false);
 		//DriveDistance driveDistance = new DriveDistance(westCoast, 8);
-		//Scheduler.getInstance().add(driveDistance);
+		//Scheduler.getInstance().add(driveDistance);*/
 	}
 
 	public void autonomousPeriodic(){
-		westCoast.arcLoop(false);
+		/*westCoast.arcLoop(false);
 		//Scheduler.getInstance().run();
 		PCM.compressorRegulate();
-		westCoast.outputToSmartDashboard();
+		westCoast.outputToSmartDashboard();*/
 	}
 
 	@Override
 	public void testInit() {
-		System.out.println("TEST MODE INIT");
+		/*System.out.println("TEST MODE INIT");
 		westCoast.pidc.getGyro().reset();
 		westCoast.setGyroControl(true);
 		westCoast.zeroSensors();
-		westCoast.debug = true;
+		westCoast.debug = true;*/
 	}
 
 	@Override
 	public void testPeriodic() {
-		// Limit for the current velocity for the robot without cascade is 3000
+		/*// Limit for the current velocity for the robot without cascade is 3000
 		try {
 			westCoast.updatePID();
 			westCoast.setVelocity(-1500, -1500); // test velocity
@@ -128,6 +152,6 @@ public class Robot extends IterativeRobot {
 		}
 		catch (Exception e) {
 			//NOTHING
-		}
+		}*/
 	}
 }

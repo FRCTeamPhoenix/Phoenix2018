f28af275113a2af7760f2541cb8f0b95fe84075a
UPIMAN73 <joshcalzadi@gmail.com>
2018-02-17 16:31:42 -0500

    Updated velocity constants

--- src/org/usfirst/frc/team2342/util/Constants.java
+++ src/org/usfirst/frc/team2342/util/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
 	public static final double JOYSTICK_DEADZONE = 0.025;
 
 	// Scales the speed of velocity mode (in rps)
-	public static final double WESTCOAST_MAX_SPEED = TALON_SPEED_RPS * 15.0d; // 2.5 origionally
+	public static final double WESTCOAST_MAX_SPEED = TALON_SPEED_RPS * 5.95d; // 2.5 origionally
 	public static final double WESTCOAST_HALF_SPEED = WESTCOAST_MAX_SPEED * 0.5d;
 
 	public static final int PCM_PORT = 11;
@@ -81,7 +81,7 @@ public class Constants {
 	public static final double Kd = 0.0d;
 	
 	// PID for Gyro TIP Turn In Place
-	public static final double tKp = 0.02d;
-	public static final double tKi = 0.0d;
+	public static final double tKp = 0.1d;
+	public static final double tKi = 0.001d;
 	public static final double tKd = 0.0d;
 }

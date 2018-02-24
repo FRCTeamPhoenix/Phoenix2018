90629bb9bfa7e1cb27da7572b8c283be45f6b9cf
armedturret <rjwhitenh@outlook.com>
2018-02-24 12:40:21 -0500

    Commented some code

--- src/org/usfirst/frc/team2342/commands/DriveDistance.java
+++ src/org/usfirst/frc/team2342/commands/DriveDistance.java
@@ -9,24 +9,29 @@ public class DriveDistance extends Command {
 	private double m_distance;
 	
 	public DriveDistance(WestCoastTankDrive westCoast, double distance) {
+		//only one system can use westCoast
 		requires(westCoast);
 		m_westCoast = westCoast;
 		m_distance = distance;
 	}
 	
 	protected void initialize() {
+		//sets distance when created
 		m_westCoast.goDistance(m_distance);
 	}
 	
 	protected void execute() {
+		//adjusts velocity values and called on scheduler.getInstance().run()
 		m_westCoast.distanceLoop();
 	}
 	
 	public boolean isFinished(){
+		//should the command terminate
     	return m_westCoast.isDistanceFinished();
     }
 	
 	protected void end() {
+		//stop moving
 		m_westCoast.setVelocity(0, 0);
 	}
 	

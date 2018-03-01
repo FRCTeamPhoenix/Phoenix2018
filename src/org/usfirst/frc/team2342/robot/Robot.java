e1dccc051b07f4e41e479d121d8e765aabf6a0bd
Ian Coolidge <ian.coolidge@asdnh.org>
2018-03-01 08:35:53 -0500

    quality of life changes to robot

--- src/org/usfirst/frc/team2342/robot/Robot.java
+++ src/org/usfirst/frc/team2342/robot/Robot.java
@@ -24,7 +24,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoSink;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -39,7 +38,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
-	Joystick gamepad;
+	
 	PCMHandler PCM;
 	WPI_TalonSRX talonFR;
 	WPI_TalonSRX talonFL;
@@ -49,11 +48,9 @@ public class Robot extends IterativeRobot {
 	WPI_TalonSRX talonIntakeRight;
 	WPI_TalonSRX talonIntakeLeft;
 	WPI_TalonSRX talonTip;
-	//Solenoid solenoidLowGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_LOWGEAR);
-	//Solenoid solenoidHighGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_HIGHGEAR);
-	Solenoid solenoid1;
+	Solenoid solenoidManipulator;
 	WestCoastTankDrive westCoast;
-	Joystick joystickR;
+	Joystick taranis;
 	Joystick XBOX;
 	CascadeElevator cascadeElevator;
 	BoxManipulator boxManipulator;
@@ -65,7 +62,7 @@ public class Robot extends IterativeRobot {
 	VideoSink server;
 
 	public Robot() {
-		gamepad = new Joystick(0);
+		taranis = new Joystick(0);
 		PCM = new PCMHandler(11);
 		talonFR = new WPI_TalonSRX(Constants.RIGHT_MASTER_TALON_ID);
 		talonFL = new WPI_TalonSRX(Constants.LEFT_MASTER_TALON_ID);
@@ -75,21 +72,14 @@ public class Robot extends IterativeRobot {
 		talonIntakeRight = new WPI_TalonSRX(Constants.TALON_INTAKE_RIGHT);
 		talonIntakeLeft = new WPI_TalonSRX(Constants.TALON_INTAKE_LEFT);
 		talonTip = new WPI_TalonSRX(Constants.TALON_TIP);
-		//Solenoid solenoidLowGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_LOWGEAR);
-		//Solenoid solenoidHighGear = new Solenoid(Constants.PCM_CAN_ID ,Constants.PCM_SLOT_HIGHGEAR);
-		solenoid1 = new Solenoid(Constants.PCM_CAN_ID, Constants.PCM_BOX_MANIPULATOR);
+		solenoidManipulator = new Solenoid(Constants.PCM_CAN_ID, Constants.PCM_BOX_MANIPULATOR);
 		westCoast = new WestCoastTankDrive(PCM, talonFL, talonFR, talonBL, talonBR);
-		joystickR = new Joystick(2);
 		XBOX = new Joystick(1);
 		cascadeElevator = new CascadeElevator(talonCascade);
-		boxManipulator = new BoxManipulator(talonIntakeRight, talonIntakeLeft, talonTip, solenoid1);
+		boxManipulator = new BoxManipulator(talonIntakeRight, talonIntakeLeft, talonTip, solenoidManipulator);
 		talonPID = new PIDGains();
-		//camera0 = CameraServer.getInstance().startAutomaticCapture(0);
-		//camera1 = CameraServer.getInstance().startAutomaticCapture(1);
-		//server = CameraServer.getInstance().getServer();
-		//server.setSource(camera0);
 		
-		// set TalonPid
+		// initialize PID gains for west coast drive
 		talonPID.p     = Constants.dtKp;
 		talonPID.i     = Constants.dtKi;
 		talonPID.d     = Constants.dtKd;
@@ -107,78 +97,57 @@ public class Robot extends IterativeRobot {
 
 	public void teleopInit() {
 		westCoast.debug = false;
-		talonPID.p     = Constants.dtKp;
-		talonPID.i     = Constants.dtKi;
-		talonPID.d     = Constants.dtKd;
-		talonPID.ff    = Constants.dtKff;
-		talonPID.rr    = 0;
-		talonPID.izone = Constants.dtKizone;
-		westCoast.updateTalonPID(0, talonPID);
-		System.out.println("TELEOP MODE INIT");
+		
+		//initialize talons to brake mode and enable compressor
 		talonFR.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 0.0, 0, 0, 0);
 		talonFL.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 0.0, 0, 0, 0);
 		PCM.turnOn();
-		Command driveJoystick = new DriveGamepad(gamepad, westCoast);
-		edu.wpi.first.wpilibj.command.Scheduler.getInstance().add(driveJoystick);
+
+		//add joystick drive to the command queue
+		Scheduler.getInstance().add(new DriveGamepad(taranis, westCoast));
+		
 		westCoast.setGyroControl(false);
 		this.updatePID();
-		//		//Camera indexes
-		//		int[] indexes = {0, 1, 2};
-		//
-		//		//Start up cameras
-		//		CameraControl cameras = new CameraControl(indexes, 640, 480);
 
-		//westCoast.debug = true;
-
-		talonFR.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 0.0, 0, 0, 0);
-		talonFL.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 0.0, 0, 0, 0);
 		talonTip.setSelectedSensorPosition(0, 0, 10);
 	}
 
 	public void teleopPeriodic() {
-		//		if (gamepad.getRawButton(5)) {
-		//			PullBox pullBox = new PullBox(boxManipulator, gamepad);
-		//			Scheduler.getInstance().add(pullBox);
-		//		}
-		//		
-		//		if (gamepad.getRawButton(6)) {
-		//			PushBox pushBox = new PushBox(boxManipulator, gamepad);
-		//			Scheduler.getInstance().add(pushBox);
-		//		}
-		this.updatePID();
-		edu.wpi.first.wpilibj.command.Scheduler.getInstance().run();
-		//Drive with joystick control in velocity mode
-		//westCoast.outputToSmartDashboard();
-		//Buttons 8 & 9 or (gamepad) 5 & 6 are Low & High gear, respectively
+		//uncomment to load PID values from network tables/sliders
+		//this.updatePID();
 		
-		if(gamepad.getRawButton(1))
+		//change camera source
+		if(taranis.getRawButton(1))
 			server.setSource(camera0);
-		if(gamepad.getRawButton(2))
+		if(taranis.getRawButton(2))
 			server.setSource(camera1);
 		
-		if (gamepad.getRawButton(Constants.LOGITECH_LEFTBUMPER))
+		//shift gears based on taranis input
+		if (taranis.getRawButton(Constants.LOGITECH_LEFTBUMPER))
 			westCoast.setLowGear();
-		else if (gamepad.getRawButton(Constants.LOGITECH_RIGHTBUMPER))
+		else if (taranis.getRawButton(Constants.LOGITECH_RIGHTBUMPER))
 			westCoast.setHighGear();
 		else
 			westCoast.setNoGear();
 		
+		//pitch box manipulator according to XBOX input.
+		//careful: hard talon limits are currently the only stop
 		if(Math.abs(XBOX.getRawAxis(Constants.XBOX_LEFTSTICK_YAXIS)) > 0.1) {
 			double speed = XBOX.getRawAxis(Constants.XBOX_LEFTSTICK_YAXIS);
 			if(speed < 0)
 				speed /= 10;
-			talonTip.set(ControlMode.PercentOutput, -XBOX.getRawAxis(Constants.XBOX_LEFTSTICK_YAXIS));
+			talonTip.set(ControlMode.PercentOutput, XBOX.getRawAxis(Constants.XBOX_LEFTSTICK_YAXIS));
 		}
 		else
 			talonTip.set(ControlMode.PercentOutput, 0);
 		
+		//control cascade according to joystick input
 		if (Math.abs(XBOX.getRawAxis(Constants.XBOX_RIGHTSTICK_YAXIS)) > Constants.CASCADE_DEADZONE) {
 			double s = XBOX.getRawAxis(Constants.XBOX_RIGHTSTICK_YAXIS);
 			double max = s < 0 ? 800 : 600;
-			System.out.println(s);
-			
 			cascadeElevator.setVelocity(s * max);
 		}
+		//control cascade from preset positions
 		else if(XBOX.getRawButton(Constants.XBOX_A))
 			edu.wpi.first.wpilibj.command.Scheduler.getInstance().add(new CascadePosition(cascadeElevator, Constants.CASCADE_BASE, XBOX));
 		else if(XBOX.getRawButton(Constants.XBOX_X))
@@ -190,11 +159,13 @@ public class Robot extends IterativeRobot {
 		else if(!cascadeElevator.isRunningPreset())
 			cascadeElevator.setVelocity(0);
 		
+		//open and close box manipulator
 		if(XBOX.getRawButton(Constants.XBOX_LEFTBUMPER))
-			solenoid1.set(true);
+			solenoidManipulator.set(true);
 		else if(XBOX.getRawButton(Constants.XBOX_RIGHTBUMPER))
-			solenoid1.set(false);
+			solenoidManipulator.set(false);
 		
+		//run intake wheels
 		if(Math.abs(XBOX.getRawAxis(Constants.XBOX_LEFTTRIGGER)) > 0.1) {
 			boxManipulator.talonIntakeRight.set(ControlMode.PercentOutput, 0.5);
 			boxManipulator.talonIntakeLeft.set(ControlMode.PercentOutput, -0.5);
@@ -206,79 +177,31 @@ public class Robot extends IterativeRobot {
 			boxManipulator.talonIntakeRight.set(ControlMode.PercentOutput, 0);
 			boxManipulator.talonIntakeLeft.set(ControlMode.PercentOutput, 0);
 		}
-		/*Scheduler.getInstance().run();
-    	//Drive with joystick control in velocity mode
-		westCoast.outputToSmartDashboard();
-		//Buttons 8 & 9 or (gamepad) 5 & 6 are Low & High gear, respectively
-		if (gamepad.getRawButton(5))
-			westCoast.setLowGear();
-		else if (gamepad.getRawButton(6))
-			westCoast.setHighGear();
-		else
-			westCoast.setNoGear();
-		//Sleep for 0.01s
-		try {
-		    Thread.sleep(100);
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		//teliopInity
-		if (joystick1.getRawButton(1)) {
-			talon1.goDistance(0.25, 0.4);
-			talon2.goDistance(-0.25, 0.4);
-			talon3.goDistance(0.25, 0.4);
-			talon4.goDistance(-0.25, 0.4);
-		}
-
-		// PCM.turnOn();
-		// WPI_TalonSRX talon1 = new WPI_TalonSRX(0);
-		// WPI_TalonSRX talon2 = new WPI_TalonSRX(1);
-		// boxManipulator = new BoxManipulator(talon1, talon2, PCM);
-		// cascadeElevator = new CascadeElevator(talon1, talon2);
-
-		 */	
-		//Scheduler.getInstance().run();
+		
+		//run all queued actions
+		Scheduler.getInstance().run();
+		
 		try {
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 
 	public void disabledInit() {
+		//upon disabling, kill west coast and remove commands from Scheduler
 		westCoast.setVelocity(0.0d, 0.0d);
 		westCoast.zeroSensors();
-		edu.wpi.first.wpilibj.command.Scheduler.getInstance().removeAll();
+		Scheduler.getInstance().removeAll();
 	}
 
 	public void autonomousInit() {
 		talonFR.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 1.0, 1, 0, 0);
 		talonFL.configSetParameter(ParamEnum.eOnBoot_BrakeMode, 1.0, 1, 0, 0);
-		// set TalonPid
-		talonPID.p     = Constants.dtKp;
-		talonPID.i     = Constants.dtKi;
-		talonPID.d     = Constants.dtKd;
-		talonPID.ff    = Constants.dtKff;
-		talonPID.rr    = Constants.dtKrr;
-		talonPID.izone = Constants.dtKizone;
-		westCoast.updateTalonPID(0, talonPID);
-		System.out.println("AUTOMODE INIT");
-		//Command goForward = new DriveForward(20, westCoast, 6.0 * Constants.TALON_SPEED_RPS);
-		//Scheduler.getInstance().add(goForward);
-		//westCoast.goArc(8, 90, 0.425, 0.5, false);
-		//DriveDistance driveDistance = new DriveDistance(westCoast, 8);
-		//Scheduler.getInstance().add(driveDistance);
-//		westCoast.goArc(4, 90, -1.0d, -1.0d, false);
-		//westCoast.updatePID();
+		
 		FMS.init();
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		
     	//calculate auto mode
     	switch(FMS.getPosition()){
     	case 1:
@@ -336,49 +259,16 @@ public class Robot extends IterativeRobot {
 			Scheduler.getInstance().add(new DriveDistance(westCoast, 10));
 			break;
     	}
-		//Command cascadeGo = new CascadePosition(cascadeElevator, 35);
-		//Scheduler.getInstance().add(cascadeGo);
-		//edu.wpi.first.wpilibj.command.Scheduler.getInstance().add(new rightswitchright(westCoast));
+		
 		try {
 			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
 		}
-		
-		//westCoast.setGyroControl(true);
-		//westCoast.pidc.gyroReset();
-		//westCoast.zeroSensors();
-		//westCoast.debug = false;
-		//westCoast.turnSet(-90.0d);
-		//this.speed = SmartDashboard.getNumber("DB/Slider 3", 0);
-		westCoast.debug = false;
-		//westCoast.updateGyroPID();
-		//double vel = -300 * speed;
-		//westCoast.setVelocity(vel, vel);
-		this.updatePID();
-		//TalonNWT.updateGyroPID(westCoast.pidc);
 	}
 
 	public void autonomousPeriodic(){
-		/*westCoast.arcLoop(false);
-		//Scheduler.getInstance().run();
-		PCM.compressorRegulate();*/
 		this.updatePID();
-		/*try {
-			this.updatePID();
-			double vel = -300 * speed;
-			this.speed = SmartDashboard.getNumber("DB/Slider 3", 0);
-			westCoast.setVelocity(vel, vel);
-			//westCoast.updateGyroPID();
-			//Scheduler.getInstance().run();
-			//westCoast.updateGyroPID();
-			//TalonNWT.updateGyroPID(westCoast.pidc);
-			Thread.sleep(10);
-		}
-		catch (Exception e) {
-
-		}*/
 		Scheduler.getInstance().run();
 	}
 

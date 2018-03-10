2828c3c7e39329a3054ebcfa3e6e1408c2c63359
Ian Coolidge <ian.coolidge@asdnh.org>
2018-03-10 12:19:20 -0500

    removed a few unnecessary classes

--- src/org/usfirst/frc/team2342/robot/Camera.java
+++ src/org/usfirst/frc/team2342/robot/Camera.java
@@ -1,11 +1,5 @@
 package org.usfirst.frc.team2342.robot;
 
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
@@ -15,63 +9,12 @@ public class Camera{
     
 	//private CvSink cvSink;
 	
-	public Camera(int resolutionX, int resolutionY, int cameraIndex, int fps, CameraServer server){
-		
-		/*camera = CameraServer.getInstance().startAutomaticCapture(cameraName, cameraIndex);
-	    camera.setResolution(resolutionX, resolutionY);
-	    camera.setFPS(60);*/
-		
+	public Camera(int resolutionX, int resolutionY, int cameraIndex, int fps, CameraServer server){		
 		camera = server.startAutomaticCapture(cameraIndex);
 		camera.setResolution(resolutionX, resolutionY);
 		camera.setFPS(fps);
 		camera.setExposureAuto();
-		
-		
-	    //cvSink = CameraServer.getInstance().getVideo(camera);
 	}
 	
-	/*public UsbCamera getCamera(){
-		return camera;
-	}*/
-}
-
-
-
-/*public class Camera implements Runnable{
-
-private UsbCamera camera;
-
-private CvSink cvSink;
-private CvSource outputStream;
-
-private Mat source;
-private Mat output;
-
-public Camera(int resolutionX, int resolutionY, String cameraName, int cameraIndex, CvSource out){
 	
-	camera = CameraServer.getInstance().startAutomaticCapture(cameraName, cameraIndex);
-    camera.setResolution(resolutionX, resolutionY);
-    
-    camera.setFPS(30);
-    
-    cvSink = CameraServer.getInstance().getVideo(camera);
-    outputStream = out;
-    
-    
-    source = new Mat(resolutionX, resolutionY, CvType.CV_32FC3);
-    output = new Mat();
 }
-
-//Run the camera
-public void run(){
-    
-    while(!Thread.interrupted()) {
-    	cvSink.setEnabled(true);
-        cvSink.grabFrame(source);
-        outputStream.putFrame(source);
-        //Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-    }
-    System.out.println("im out here");
-    cvSink.setEnabled(false);
-}
-}*/

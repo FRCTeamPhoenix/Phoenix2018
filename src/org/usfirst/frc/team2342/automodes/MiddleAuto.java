d8d99af9e35a9cbdc720e498fa1709e8f351b51d
Ian Coolidge <ian.coolidge@asdnh.org>
2018-04-06 18:08:22 -0400

    comments to allow for easier tuning of MiddleAuto

--- src/org/usfirst/frc/team2342/automodes/MiddleAuto.java
+++ src/org/usfirst/frc/team2342/automodes/MiddleAuto.java
@@ -8,49 +8,30 @@ import org.usfirst.frc.team2342.robot.subsystems.CascadeElevator;
 import org.usfirst.frc.team2342.robot.subsystems.TankDrive;
 import org.usfirst.frc.team2342.util.Constants;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class MiddleAuto extends CommandGroup {
 
 	public MiddleAuto(TankDrive drive, CascadeElevator cascade, BoxManipulator manip, Joystick gamepad) {
-		/*if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
-			addSequential(new DriveDistance(drive, 0.66));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, 34, drive));
-			addParallel(new CascadePosition(cascade, Constants.CASCADE_SWITCH, gamepad));
-			addSequential(new DriveDistance(drive, 8.33));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, -34, drive));
-			//addSequential(new DriveDistance(drive, 0.66));
-			addSequential(new PushBox(manip, gamepad, 1.0));
-		} else {
-			addSequential(new DriveDistance(drive, 0.66));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, -34, drive));
-			addParallel(new CascadePosition(cascade, Constants.CASCADE_SWITCH, gamepad));
-			addSequential(new DriveDistance(drive, 8.33));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, 34, drive));
-			//addSequential(new DriveDistance(drive, 0.66));
-			addSequential(new PushBox(manip, gamepad, 1.0));
-		}*/
+		
+		//Drive forward. Second paramater is drive time (ms) and third parameter is voltage (percentage 0.0 to 1.0).
 		addSequential(new DriveVoltageTime(drive, 2000, 0.3));
+		
+		//Raise cascade to SWITCH HEIGHT.
 		addSequential(new CascadePosition(cascade, Constants.CASCADE_SWITCH, gamepad));
-		addSequential(new PushBox(manip, gamepad));
-		/*if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, 34, drive));
-			addParallel(new CascadePosition(cascade, Constants.CASCADE_SWITCH, gamepad));
-			addSequential(new DriveDistance(drive, 8.33));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, -34, drive));
-			//addSequential(new DriveDistance(drive, 2));
-			//addSequential(new PushBox(manip, gamepad, 0.5));
-		} else {
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, -34, drive));
-			addParallel(new CascadePosition(cascade, Constants.CASCADE_SWITCH, gamepad));
-			addSequential(new DriveDistance(drive, 8.33));
-			addSequential(new TurnAngle(Constants.WESTCOAST_TURN_SPEED, 34, drive));
-			//addSequential(new DriveDistance(drive, 1));
-			//addSequential(new PushBox(manip, gamepad, 0.5));
+		
+		//This if statement does logic to determine if we should push a cube into the switch.
+		//If DB/Button 1 is PRESSED, we will be on the LEFT side.
+		//If DB/Button 1 is NOT PRESSED, we will be on the RIGHT side.
+		//This should not need to be changed significantly.
+		if(!(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L' ^ SmartDashboard.getBoolean("DB/Button 1", false))) {
+			
+			//Push a box into the switch. The third parameter is the shooting power (percentage 0.0 to 1.0).
+			addSequential(new PushBox(manip, gamepad, 0.5));
 		}
-		addSequential(new DriveDistance(drive, -2));
-		addSequential(new CascadePosition(cascade, Constants.CASCADE_BASE, gamepad));*/
 		
 	}
 	

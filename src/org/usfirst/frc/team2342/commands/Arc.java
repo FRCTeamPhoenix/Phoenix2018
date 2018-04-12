122c99c0a3a86735e835c56806f9323b05cb1a9f
denver-blake <denver.blake26@gmail.com>
2018-04-12 10:24:22 -0400

    created arc command

---
+++ src/org/usfirst/frc/team2342/commands/Arc.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team2342.commands;
+
+import org.usfirst.frc.team2342.PIDLoops.GyroPIDController;
+import org.usfirst.frc.team2342.robot.sensors.Gyro;
+import org.usfirst.frc.team2342.robot.subsystems.TankDrive;
+import org.usfirst.frc.team2342.util.Constants;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Arc extends Command {
+
+	private TankDrive tankDrive;
+	private double angle;
+	private double currentAngle;
+	private double distance;
+    public Arc(TankDrive tankDrive,double distance, double angle) {
+    	this.tankDrive = tankDrive;
+    	this.angle = angle;
+    	this.distance = distance;
+    	requires(tankDrive);
+    	
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	currentAngle = distance / tankDrive.leftA.getSelectedSensorPosition(0) * angle;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	currentAngle = distance / tankDrive.leftA.getSelectedSensorPosition(0) * angle;
+    	
+    	tankDrive.setVelocity(Constants.WESTCOAST_HALF_SPEED * ( 1 - .1 * (currentAngle - Gyro.angle())), Constants.WESTCOAST_HALF_SPEED);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return Math.abs((tankDrive.leftA.getSelectedSensorPosition(0)) + distance) < 300;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
